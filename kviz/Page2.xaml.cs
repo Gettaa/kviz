e27aef68edad94c40dc2b8c5e4d756a1040609e5
wolfb25 <fbalint0126@gmail.com>
2024-02-01 09:19:00 +0100

    sok cucc

--- kviz/Page2.xaml.cs
+++ kviz/Page2.xaml.cs
@@ -14,57 +14,45 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace kviz {
-	/// <summary>
-	/// Interaction logic for Page2.xaml
-	/// </summary>
 	public partial class Page2 : Page {
 
-		private int kerdesAllas = 0;
 		public Jatek jatek = new Jatek(Page1.ValasztottJatekos, Page1.ValasztottKat);
+		private List<RadioButton> radioButtons = [];
+		private List<CheckBox> checkBoxes = [];
 
 		public Page2() {
 			InitializeComponent();
+			radioButtons.Add(valasz1); radioButtons.Add(valasz2); radioButtons.Add(valasz3);
+			checkBoxes.Add(valasz1t); checkBoxes.Add(valasz2t); checkBoxes.Add(valasz3t);
 			updateContent();
 		}
 
 		private void tovabb_Click(object sender, RoutedEventArgs e) {
-			nextRound();
+			Next();
+			updateContent();
+		}
+
+		private void Next() {
+			if (jatek.JelenKerdes().TobbValasz) jatek.Next(checkBoxes.Where(c => c.IsChecked == true).Select(c => c.Content.ToString()).ToList());
+			else jatek.Next(radioButtons.Where(c => c.IsChecked == true).First().Content.ToString());
 		}
 
 		private void updateContent() {
-			kerdesszam.Content = $"{kerdesAllas+1}/10";
-			if (kerdesAllas < 10) {
-				Kerdes jelenKerdes = jatek.Kerdesek[kerdesAllas];
-				kerdes.Content = jelenKerdes.Szoveg;
-				if (jelenKerdes.TobbValasz) {
-					valasz1.Visibility = Visibility.Hidden;
-					valasz2.Visibility = Visibility.Hidden;
-					valasz3.Visibility = Visibility.Hidden;
-					valasz1t.Visibility = Visibility.Visible;
-					valasz2t.Visibility = Visibility.Visible;
-					valasz3t.Visibility = Visibility.Visible;
-					valasz1t.Content = jelenKerdes.KevertValaszok[0];
-					valasz2t.Content = jelenKerdes.KevertValaszok[1];
-					valasz3t.Content = jelenKerdes.KevertValaszok[2];
-				}
-				else {
-					valasz1.Visibility = Visibility.Visible;
-					valasz2.Visibility = Visibility.Visible;
-					valasz3.Visibility = Visibility.Visible;
-					valasz1t.Visibility = Visibility.Hidden;
-					valasz2t.Visibility = Visibility.Hidden;
-					valasz3t.Visibility = Visibility.Hidden;
-					valasz1.Content = jelenKerdes.KevertValaszok[0];
-					valasz2.Content = jelenKerdes.KevertValaszok[1];
-					valasz3.Content = jelenKerdes.KevertValaszok[2];
+			kerdesszam.Content = $"{jatek.KerdesAllas+1}/10";
+			kerdes.Content = jatek.JelenKerdes().Szoveg;
+			if (jatek.JelenKerdes().TobbValasz) {
+				radioButtons.ForEach(r => r.Visibility = Visibility.Hidden);
+				checkBoxes.ForEach(c => c.Visibility = Visibility.Visible);
+				for (int i = 0; i < 3; i++) {
+					checkBoxes[i].Content = jatek.JelenKerdes().KevertValaszok[i];
 				}
 			}
-		}
-
-		private void nextRound() {
-			if (kerdesAllas < 9) {
-				kerdesAllas++;
-				updateContent();
+			else {
+				radioButtons.ForEach(r => r.Visibility = Visibility.Visible);
+				checkBoxes.ForEach(c => c.Visibility = Visibility.Hidden);
+				for (int i = 0; i < 3; i++) {
+					radioButtons[i].Content = jatek.JelenKerdes().KevertValaszok[i];
+				}
 			}
 		}
 	}
--- kviz/jatek.cs
+++ kviz/jatek.cs
@@ -14,15 +14,38 @@ namespace kviz {
 		public Jatekos Embi {  get; private set; }
 		public char Temakor { get; private set; }
 		public Kerdes[] Kerdesek { get; private set; }
+		public int KerdesAllas { get; private set; } = 0;
+		public int JoTipp { get; private set; } = 0;
+		public int RosszTipp { get; private set; } = 0;
+		public Kerdes JelenKerdes() { return Kerdesek[KerdesAllas]; }
 
 		public Jatek(Jatekos j, char c) {
 			Embi = j;
 			Temakor = c;
-			Kerdesek = Adatok.Kerdesek
+			Kerdesek =
+				Adatok.Kerdesek
 				.Where(k => k.Kategoria == Temakor)
 				.OrderBy(k => Guid.NewGuid())
 				.Take(10)
-				.ToArray();
+				.ToArray()
+			;
+		}
+
+		public void Next(string szoveg) {
+			if (KerdesAllas < 9 && Kerdesek[KerdesAllas++].HelyesTipp(szoveg)) JoTipp++;
+			RosszTipp = KerdesAllas - JoTipp;
+            Console.WriteLine($"Jotipp: {JoTipp} Rossztipp : {RosszTipp} Kerdesallas: {KerdesAllas}");
+        }
+
+		public void Next(List<string> szovegek) {
+			bool j = true;
+			if (KerdesAllas < 9) {
+				foreach (string s in szovegek) if (!JelenKerdes().KevertValaszok.Contains(s)) j = false;
+				if (j) JoTipp++;
+				KerdesAllas++;
+			}
+			RosszTipp = KerdesAllas - JoTipp;
+			Console.WriteLine($"Jotipp: {JoTipp} Rossztipp : {RosszTipp} Kerdesallas: {KerdesAllas}");
 		}
 	}
 }
